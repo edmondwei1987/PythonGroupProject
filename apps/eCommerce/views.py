05597b3ad13e6a9e0e893e0978de05ea520e65b5
Peng Wei <pengwei@PengdeMacBook-Pro.local>
2018-02-28 14:06:51 -0800

    add html files

--- apps/eCommerce/views.py
+++ apps/eCommerce/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def admin(request):
+    return render(request,'eCommerce/admin_orders.html')
+
+def admin_index(request):
+    return render(request,'eCommerce/admin_index.html')
