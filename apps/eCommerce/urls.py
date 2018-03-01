497adfa6041328f2abe675333c3dfdb84c0d038d
Pookie <pookie@Nicks-MacBook-Pro.local>
2018-02-28 16:06:12 -0800

    update

--- apps/eCommerce/urls.py
+++ apps/eCommerce/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^admin_login$',views.admin_login),
-    url(r'^admin_dashboard$',views.admin_dashboard),
-    url(r'^admin_index$',views.admin_index),
-    url(r'^admin/dashboard$',views.admin),
+    
     url(r'^admin/index$',views.admin_index),
+    url(r'^admin_login$',views.admin_login),
+    url(r'^admin/dashboard$',views.admin_dashboard),
     url(r'^admin/products$',views.admin_products),
     url(r'^admin/orderdetail$',views.admin_orderdetail),
     url(r'^admin/productdetail$',views.admin_productdetail),
--- apps/eCommerce/views.py
+++ apps/eCommerce/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+from models import *
+from django.contrib import messages
 
+def admin_index(request):
+    #login
+    return render(request,'eCommerce/admin_index.html')
 
 # username: admin
 # password: python
@@ -12,26 +17,21 @@ def admin_login(request):
         admin = Admin.objects.get(username = request.POST['username'])
     except:
         messages.error(request,'username or Password incorrect.')
-        return redirect('/admin')
+        return redirect('/admin/index')
     # check if password correct
     if (request.POST['password'], admin.password):
         request.session['admin_id'] = admin.id
-        return redirect('/admin_dashboard')
+        return redirect('/admin/dashboard')
     else:
         messages.error(request,'Email or Password incorrect.')
-        return redirect('/admin')
-
-def admin_dashboard(request):
-    return render(request,'eCommerce/admin_orders.html')
+        return redirect('/admin/index')
 
 # Create your views here.
-def admin(request):
+def admin_dashboard(request):
     #show all the orders on dashboard
     return render(request,'eCommerce/admin_orders.html')
 
-def admin_index(request):
-    #login
-    return render(request,'eCommerce/admin_index.html')
+
 
 def admin_products(request):
     #show all the products
