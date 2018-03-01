c1290f5e26686b00b595434e5f8307f3f0a30e4f
Sindy <sindyxin1@gmail.com>
2018-02-28 16:46:14 -0800

    update

--- apps/eCommerce/views.py
+++ apps/eCommerce/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def product(request):
+    return render(request, 'customer/product.html')
