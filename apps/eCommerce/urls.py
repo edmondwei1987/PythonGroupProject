8b4fd231462a9961684234caaf60be25f7053236
Peng Wei <pengwei@PengdeMacBook-Pro.local>
2018-02-28 16:10:25 -0800

    update

--- apps/eCommerce/urls.py
+++ apps/eCommerce/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^admin/dashboard$',views.admin),
+    url(r'^admin/dashboard$',views.admin_dashboard),
     url(r'^admin/index$',views.admin_index),
     url(r'^admin/products$',views.admin_products),
     url(r'^admin/orderdetail$',views.admin_orderdetail),
